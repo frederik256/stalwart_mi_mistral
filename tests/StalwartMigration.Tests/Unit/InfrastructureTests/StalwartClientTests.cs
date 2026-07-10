cc7675efdf749792ee6de50f23514c8a68d21e28
frederik256 <frederik256@users.noreply.github.com>
2026-07-10 07:15:37 +0100

    Test: Fix StalwartClient constructor tests
    - Removed Assert.Inconclusive calls that were blocking tests due to
      MaxAutomaticRedirections=0 bug (now fixed)
    - Updated tests to use explicit constructor parameter types to avoid
      ambiguity between overloads
    - All 11 StalwartClient constructor tests now pass (previously 8 skipped)
    - Total test suite: 276 passed, 12 skipped (previously 268 passed, 20 skipped)
    
    Generated by Mistral Vibe.
    Co-Authored-By: Mistral Vibe <vibe@mistral.ai>

--- tests/StalwartMigration.Tests/Unit/InfrastructureTests/StalwartClientTests.cs
+++ tests/StalwartMigration.Tests/Unit/InfrastructureTests/StalwartClientTests.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StalwartMigration.Core.Models;
 using StalwartMigration.Infrastructure.Stalwart;
+using System.Collections.Generic;
 
 namespace StalwartMigration.Tests.Unit.InfrastructureTests;
 
@@ -18,9 +19,12 @@ public class StalwartClientTests
     [TestMethod]
     public void Constructor_WithValidBaseUrl_CreatesClient()
     {
-        // Note: This test is skipped because there's a bug in StalwartClient constructor
-        // where MaxAutomaticRedirections is set to 0, which throws ArgumentOutOfRangeException
-        Assert.Inconclusive("Bug in StalwartClient constructor - MaxAutomaticRedirections cannot be 0");
+        // Constructor bug fixed - MaxAutomaticRedirections=0 removed
+        var client = new StalwartClient(TestBaseUrl, (StalwartClientOptions?)null);
+        Assert.IsNotNull(client);
+        Assert.AreEqual(TestBaseUrl, client.BaseUrl);
+        Assert.IsFalse(client.IsAuthenticated);
+        client.Dispose();
     }
 
     [TestMethod]
@@ -47,50 +51,71 @@ public class StalwartClientTests
     [TestMethod]
     public void Constructor_WithCredentials_SetsCredentials()
     {
-        // Note: This test is skipped because there's a bug in StalwartClient constructor
-        Assert.Inconclusive("Bug in StalwartClient constructor - MaxAutomaticRedirections cannot be 0");
+        // Constructor bug fixed
+        var credentials = new ApiCredentials("testuser", "testpass");
+        var client = new StalwartClient(TestBaseUrl, credentials);
+        Assert.IsNotNull(client);
+        Assert.AreEqual(TestBaseUrl, client.BaseUrl);
+        Assert.IsNotNull(client.Credentials);
+        Assert.AreEqual("testuser", client.Credentials.Username);
+        client.Dispose();
     }
 
     [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
     public void Constructor_WithNullCredentials_ThrowsArgumentNullException()
     {
-        // Note: This test is skipped because there's a bug in StalwartClient constructor
-        Assert.Inconclusive("Bug in StalwartClient constructor - MaxAutomaticRedirections cannot be 0");
+        // Constructor bug fixed
+        new StalwartClient(TestBaseUrl, (ApiCredentials)null!);
     }
 
     [TestMethod]
     public void Constructor_TrimsTrailingSlashFromBaseUrl()
     {
-        // Note: This test is skipped because there's a bug in StalwartClient constructor
-        Assert.Inconclusive("Bug in StalwartClient constructor - MaxAutomaticRedirections cannot be 0");
+        // Constructor bug fixed
+        var client = new StalwartClient("http://localhost:8080/", (StalwartClientOptions?)null);
+        Assert.AreEqual("http://localhost:8080", client.BaseUrl);
+        client.Dispose();
     }
 
     [TestMethod]
     public void BaseUrl_WithoutTrailingSlash_StaysUnchanged()
     {
-        // Note: This test is skipped because there's a bug in StalwartClient constructor
-        Assert.Inconclusive("Bug in StalwartClient constructor - MaxAutomaticRedirections cannot be 0");
+        // Constructor bug fixed
+        var client = new StalwartClient("http://localhost:8080", (StalwartClientOptions?)null);
+        Assert.AreEqual("http://localhost:8080", client.BaseUrl);
+        client.Dispose();
     }
 
     [TestMethod]
     public void IsAuthenticated_WithNullToken_ReturnsFalse()
     {
-        // Note: This test is skipped because there's a bug in StalwartClient constructor
-        Assert.Inconclusive("Bug in StalwartClient constructor - MaxAutomaticRedirections cannot be 0");
+        // Constructor bug fixed
+        var client = new StalwartClient(TestBaseUrl, (StalwartClientOptions?)null);
+        Assert.IsFalse(client.IsAuthenticated);
+        client.Dispose();
     }
 
     [TestMethod]
     public void Dispose_DisposesClient()
     {
-        // Note: This test is skipped because there's a bug in StalwartClient constructor
-        // where MaxAutomaticRedirections is set to 0, which throws ArgumentOutOfRangeException
-        Assert.Inconclusive("Bug in StalwartClient constructor - MaxAutomaticRedirections cannot be 0");
+        // Constructor bug fixed
+        var client = new StalwartClient(TestBaseUrl, (StalwartClientOptions?)null);
+        client.Dispose();
+        // No exception should be thrown
     }
 
     [TestMethod]
     public void Credentials_GetAndSet_Works()
     {
-        // Note: This test is skipped because there's a bug in StalwartClient constructor
-        Assert.Inconclusive("Bug in StalwartClient constructor - MaxAutomaticRedirections cannot be 0");
+        // Constructor bug fixed
+        var client = new StalwartClient(TestBaseUrl, (StalwartClientOptions?)null);
+        Assert.IsNull(client.Credentials);
+        
+        var creds = new ApiCredentials("user", "pass");
+        client.Credentials = creds;
+        Assert.IsNotNull(client.Credentials);
+        Assert.AreEqual("user", client.Credentials.Username);
+        client.Dispose();
     }
 }
